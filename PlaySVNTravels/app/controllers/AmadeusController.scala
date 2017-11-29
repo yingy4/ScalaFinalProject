280e6e70917fcfa20a962cce50bdfc3f70dbe236
Nandeep <nelagondanahalli.n@husky.neu.edu>
2017-11-28 22:28:46 -0500

    Play App

---
+++ PlaySVNTravels/app/controllers/AmadeusController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import javax.inject._
+
+import play.api.libs.ws.WSClient
+import play.api.mvc._
+
+
+
+@Singleton
+class AmadeusController @Inject() (cc :ControllerComponents) (ws: WSClient) extends AbstractController(cc){
+  import scala.concurrent.ExecutionContext.Implicits._
+  val url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search"
+
+  def cheapFlights (src: String, des: String) = Action.async {
+    ws.url(url).addQueryStringParameters("apikey" -> "Gbv5AzOeVWw2c0R3r2TBdA2SJA4kZkpB","origin" -> src,"destination" -> des, "departure_date" -> "2017-12-25").get().map { response =>
+      Ok(response.body)
+    }
+  }
+
+}
