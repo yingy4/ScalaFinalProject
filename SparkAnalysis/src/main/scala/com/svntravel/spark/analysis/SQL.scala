ee085714e39b7290a50c3c4ea0f6d3d6d8f839e2
Saravan Deepak <saravandeepakv@gmail.com>
2017-11-29 21:24:59 -0500

    more changes

--- SparkAnalysis/src/main/scala/com/svntravel/spark/analysis/SQL.scala
+++ SparkAnalysis/src/main/scala/com/svntravel/spark/analysis/SQL.scala
@@ -14,39 +14,77 @@ object SQL {
 object Aggregation {
 
   def aggMaxFarePerLocation (ds: Dataset[Ticket]) : DataFrame = {
-    ds.select("year", "quarter", "origin", "destination", "fare").groupBy("year", "quarter", "origin", "destination").max()
+    ds.select("year", "quarter", "origin", "destination", "fare")
+      .groupBy("year", "quarter", "origin", "destination")
+      .max()
   }
 
   def aggMinFarePerLocation (ds: Dataset[Ticket]) : DataFrame = {
-    ds.select("year", "quarter", "origin", "destination", "fare").groupBy("year", "quarter", "origin", "destination").min()
+    ds.select("year", "quarter", "origin", "destination", "fare")
+      .groupBy("year", "quarter", "origin", "destination")
+      .min()
   }
 
   def aggAvgFarePerLocation (ds: Dataset[Ticket]) : DataFrame = {
-    ds.select("year", "quarter", "origin", "destination", "fare").groupBy("year", "quarter", "origin", "destination").mean()
+    ds.select("year", "quarter", "origin", "destination", "fare")
+      .groupBy("year", "quarter", "origin", "destination")
+      .mean()
   }
 
   def aggMeanFarePerLocation (ds: Dataset[Ticket]) : DataFrame = {
-    ds.select("year", "quarter", "origin", "destination", "fare").groupBy("year", "quarter", "origin", "destination").mean()
+    ds.select("year", "quarter", "origin", "destination", "fare")
+      .groupBy("year", "quarter", "origin", "destination")
+      .mean()
+  }
+
+  def locationAggregates (ds: Dataset[Ticket]) : DataFrame = {
+    ds.select("year", "quarter", "origin", "destination", "fare")
+      .groupBy("year", "quarter", "origin", "destination")
+      .agg(
+        min("fare").as("minFare"),
+        max("fare").as("maxFare"),
+        avg("fare").as("avgFare"),
+        stddev(col("fare").cast("double").as("stdFare"))
+      )
   }
 
   def findTopRoutes (ds: Dataset[Ticket], k: Int) : DataFrame = {
-    ds.select("origin", "destination").groupBy("origin", "destination").count().orderBy(desc("count"))
+    ds.select("year", "quarter", "origin", "destination").groupBy("year", "quarter", "origin", "destination")
+      .count()
+      .orderBy(desc("count"))
+      .limit(k)
   }
 
   def findTopOrigin (ds: Dataset[Ticket], k: Int) : DataFrame = {
-    ds.select("origin").groupBy("origin").count().orderBy(desc("count")).limit(k)
+    ds.select("year", "quarter", "origin").groupBy("year", "quarter", "origin")
+      .count()
+      .orderBy(desc("count"))
+      .limit(k)
   }
 
   def findTopDestination (ds: Dataset[Ticket], k: Int) : DataFrame = {
-    ds.select("destination").groupBy("destination").count().orderBy(desc("count")).limit(k)
+    ds.select("year", "quarter", "destination").groupBy("year", "quarter", "destination")
+      .count()
+      .orderBy(desc("count"))
+      .limit(k)
   }
 
   def findTopTicketingCarrier (ds: Dataset[Ticket], k: Int) : DataFrame = {
-    ds.select("tktCarrier").groupBy("tktCarrier").count().orderBy(desc("count")).limit(k)
+    ds.select("year", "quarter", "tktCarrier").groupBy("year", "quarter", "tktCarrier")
+      .count()
+      .orderBy(desc("count"))
+      .limit(k)
   }
 
   def findTopOperatingCarrier (ds: Dataset[Ticket], k: Int) : DataFrame = {
-    ds.select("opCarrier").groupBy("opCarrier").count().orderBy(desc("count")).limit(k)
+    ds.select("year", "quarter","opCarrier").groupBy("year", "quarter","opCarrier")
+      .count()
+      .orderBy(desc("count"))
+      .limit(k)
+  }
+
+  def roundTripPerc (df: DataFrame, ds:Dataset[Ticket]) : Double = {
+    ds.where(ds("roundTrip") === 1.0).count().toDouble / df.count().toDouble
   }
 
 }
@@ -62,6 +100,10 @@ object Join {
 object Filter {
 
   def filterDataset (df: DataFrame, couponDf: DataFrame, ticketsDF: DataFrame, marketDF: DataFrame, minFare: Double): DataFrame = {
-    df.select(couponDf("ITIN_ID"), couponDf("YEAR") , couponDf("QUARTER"), marketDF("ORIGIN"), marketDF("DEST"), ticketsDF("ROUNDTRIP"), ticketsDF("ITIN_FARE"), ticketsDF("DISTANCE"), marketDF("TICKET_CARRIER"), marketDF("OPERATING_CARRIER")).where(marketDF("ORIGIN") =!= marketDF("DEST")).where(ticketsDF("ITIN_FARE") > minFare).distinct()
+    df.select(couponDf("ITIN_ID"), couponDf("YEAR") , couponDf("QUARTER"), marketDF("ORIGIN"), marketDF("DEST"), ticketsDF("ROUNDTRIP"), ticketsDF("ITIN_FARE"), ticketsDF("DISTANCE"), marketDF("TICKET_CARRIER"), marketDF("OPERATING_CARRIER"))
+      .where(marketDF("ORIGIN") =!= marketDF("DEST"))
+      .where(ticketsDF("ITIN_FARE") > minFare)
+      .distinct()
   }
+
 }
