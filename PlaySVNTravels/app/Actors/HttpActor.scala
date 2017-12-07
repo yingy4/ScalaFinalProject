9c42ada93152756cbcaf347135b0148e8df63828
vaishak dinesh <dinesh.v@husky.neu.edu>
2017-12-07 13:05:51 -0500

    New actor cases for different requests

--- PlaySVNTravels/app/Actors/HttpActor.scala
+++ PlaySVNTravels/app/Actors/HttpActor.scala
@@ -5,31 +5,43 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import akka.util.ByteString
-import play.api.libs.json.JsValue
 
 
 case class UserRequest(src:String,dest:String,topic:String)
+case class InpsiredUserRequest(src:String, topic:String,sender:ActorRef)
+case class AggregatedUserRequest(src:String,des:String,sender:ActorRef)
 
 object HttpActor{
-  def props(out:ActorRef) = Props(new HttpActor(out))
+  def props(out:ActorRef)(src:String,des:String) = Props(new HttpActor(out)(src,des))
 }
 
 
 /** Handles HTTP Requests
   *
   * @param out an Actor reference obj which pipes in and out
+  * @param src the flight source
+  * @param des the destination of the flight
   */
 //TODO:: remove print statements, introduce logging?
-class HttpActor(out:ActorRef) extends Actor with ActorLogging{
+class HttpActor(out:ActorRef)(src:String,des:String) extends Actor with ActorLogging{
 
   val system = ActorSystem("httpSystem")
 
   val apiActor = system.actorOf(Props[APIActor])
   override def receive: Receive = {
-    case msg:String =>
-      val inputArray = msg.split("\\*")
-      out ! ("I received your message: " + inputArray(0))
-      apiActor ! UserRequest(inputArray(0),inputArray(1),Kafka.AmadeusProducer.TOPIC)
+    case "two" =>
+      println("in normal case")
+      println(s"src = ${src}")
+      //val inputArray = msg.split("\\*")
+      out ! ("I received your message: " +src)
+      apiActor ! UserRequest(src,des,Kafka.AmadeusProducer.TOPIC)
+    case "one" =>
+      println("in tuple case")
+      out ! ("I received your message: " + des)
+      apiActor ! InpsiredUserRequest(src,Kafka.AmadeusProducer.TOPIC,out)
+    case "aggregate" =>
+      println("in aggregate case")
+      apiActor ! AggregatedUserRequest(src,des,out)
   }
 }
 
@@ -52,6 +64,24 @@ class APIActor extends Actor{
       http.singleRequest(HttpRequest(uri = s"http://localhost:9000/cheapflights/${req.src}/${req.dest}")).onComplete(response =>
         response.get.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach {
           body => kafkaActor ! body.utf8String
+        }
+      )
+    case req:InpsiredUserRequest=>
+      val kafkaActor = system.actorOf(Props[KafkaActor])
+
+      // Creates HTTP Request and unwraps HttPResponse to get the response json as string
+      http.singleRequest(HttpRequest(uri = s"http://localhost:9000/inspireme/${req.src}")).onComplete(response =>
+        response.get.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach {
+          body => req.sender ! body.utf8String
+        }
+      )
+    case req:AggregatedUserRequest=>
+      val kafkaActor = system.actorOf(Props[KafkaActor])
+
+      // Creates HTTP Request and unwraps HttPResponse to get the response json as string
+      http.singleRequest(HttpRequest(uri = s"http://localhost:9000/cheapest/${req.src}/${req.des}")).onComplete(response =>
+        response.get.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach {
+          body => req.sender ! body.utf8String
         }
       )
   }
