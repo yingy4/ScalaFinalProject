4171e2f755e816de44c5d61ec39f6c0b96588ec0
vaishak dinesh <dinesh.v@husky.neu.edu>
2017-12-11 15:52:38 -0500

    Logic to start polling + logging

--- PlaySVNTravels/app/spark/StreamingCheapFlights.scala
+++ PlaySVNTravels/app/spark/StreamingCheapFlights.scala
@@ -1,16 +1,28 @@
 package spark
 
+import Actors.{ConsumerActor, ConsumerKeyActor}
 import Kafka.SparkStreamProducer
+import akka.actor.{ActorRef, Props}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import play.api.Logger
 import play.api.libs.json.Json
 import utils.CheapFlights
 
+import scala.collection.mutable.ArrayBuffer
+
+object StreamingCheapFlights{
+  val consumerKeyActor = ConsumerActor.system.actorOf(Props[ConsumerKeyActor])
+  def startPolling(key:String,actor:ActorRef) = {
+    actor ! (key,actor.path.toSerializationFormat)
+  }
+}
 
 class StreamingCheapFlights {
 
   val ssc = CommonSparkContext.sparkStreamingContext
+
 
   val topics = List("Amadeus").toSet
   val kafkaParams = Map[String, Object](
@@ -25,18 +37,29 @@ class StreamingCheapFlights {
   val cheapFlightsStream = KafkaUtils.createDirectStream[String, String](
     ssc,
     LocationStrategies.PreferConsistent,
-    ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)).map {
+    ConsumerStrategies.Subscribe[String, String](topics, kafkaParams))
+
+   val arr = new ArrayBuffer[Char]()
+   val key = cheapFlightsStream.map(record=> record.key()).flatMap(_.toString).foreachRDD({
+     rdd => arr ++= rdd.collect()
+     StreamingCheapFlights.startPolling(arr.mkString,StreamingCheapFlights.consumerKeyActor)
+   })
+
+  val analyzedData = cheapFlightsStream.map {
         import utils.CheapFlightsReads._
-    record => Json.parse(record.value()).as[CheapFlights]
+    record => {
+      Json.parse(record.value()).as[CheapFlights]
+    }
   }.flatMap(SparkStreamAnalyzer.getCheapestFlights(_ , 10))
 
-  cheapFlightsStream.foreachRDD( rdd => {
+  analyzedData.foreachRDD( rdd => {
     import utils.CheapFlightsWrites._
     val jsflights = Json.toJson(rdd.collect())
 
-    //--- Push to Kafa then to UI ----//
-    //println(msg.toString())
+    Logger.debug("Data sent back to kafka")
     SparkStreamProducer.producer.send(new ProducerRecord(SparkStreamProducer.TOPIC,"spark", jsflights.toString()))
+    Thread.sleep(1500)
+
   }
   )
 
