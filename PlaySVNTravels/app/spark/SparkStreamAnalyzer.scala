0cb21131f047333b0bf690135fdd00badc63cc9d
Nandeep <nelagondanahalli.n@husky.neu.edu>
2017-12-13 22:08:57 -0500

    Moved api key to config file and update test cases

--- PlaySVNTravels/app/spark/SparkStreamAnalyzer.scala
+++ PlaySVNTravels/app/spark/SparkStreamAnalyzer.scala
@@ -6,7 +6,7 @@ object SparkStreamAnalyzer {
 
   def getCheapestFlights(cfs: CheapFlights, n: Int) = {
     if(n < cfs.results.length)
-      cfs.results.sortBy( r => r.fare.total_price )
+      cfs.results.sortBy( r => r.fare.total_price ).take(n)
     else
       cfs.results.sortBy( r => r.fare.total_price )
   }
